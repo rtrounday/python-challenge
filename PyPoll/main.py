1b552e0825912c26b8347e7184dfdef10c1a387d
Ray <rtrounday@gmail.com>
2018-02-04 08:05:19 -0800

    Commi latest changes
    Removed debug print statements.  Added votepercent,winner etc.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,19 +5,19 @@ import csv
 os.system("cls")
  
 
-# set path for file
+# set path for input and output file
 filepath = os.path.join("Data","election_data_2.csv")
 newfilepath = os.path.join("Data","election_data_2_results.csv")
 
+
+# initialize lists
 candidates = []
 votes = []
 voteresults = []
-votePercent = []
-j = []
+votepercent = []
 
-# create a list of candiddates
 
-#print (candidates)
+
 
 # open the csv
 
@@ -26,7 +26,7 @@ with open(filepath, 'r', newline="") as csvfile:
     csvReader = csv.reader(csvfile, delimiter=",")
     
     next(csvReader,None)
-
+    # locate each unique candidate and create list of all votes
     for x in csvReader:
         if x[2] not in candidates:
             #Create candiate list
@@ -34,14 +34,69 @@ with open(filepath, 'r', newline="") as csvfile:
         else:
             votes.append(x[2])
     print (candidates)
-    #print (votes)
-    #print (votes[1:5])
-    # this provides the number of occurrences of "Khan" in the vote list
-    j.append(votes.count('Khan'))
-    # I am trying to iterate through the list of candidates capture the number of occurrences my vote list
-    for i in candidates:
+    
+    # count the votes per candidate
+        
+    for i in range(len(candidates)):
+      
+        voteresults.append(votes.count(candidates[i]))
+
+        print(voteresults)
+    # arrive at total votes cast
+    totalvotes = sum(voteresults)
+    # create a dict of votes per candidate
+    j = dict(zip(candidates, voteresults))
+    print (j)
+
+    k = dict(zip(candidates,votepercent,voteresults))
+    print (k)
+    # find the candidate with the most popular votes
+    winner = max(j, key=j.get)
+    print (winner)
+    
+    # calculate the vote percent
+    for i in range(len(voteresults)):
         # print (i)
-        j.append(votes.count('candidates[i]'))
-        print (j)
+        percent = round((voteresults[i]/totalvotes) * 100,2)
+        # print (percent)
+        votepercent.append(percent)
+    print (votepercent)
+
+    
+    cleanCSV = zip(candidates,votepercent,voteresults)
+
+    # print results to the console
+
+    print ("Election Results")
+    print ("")
+    
+    print ('*'*80)
+    print ('Total Votes: ' + str(totalvotes))
+    print ('*'*80)
+    print ("")
+
+    for i in range(len(candidates)):
+        # print (i)
+         print ((candidates[i]) + " " + str(votepercent[i]) +"%" + " "  + str(voteresults[i] ))
+        # print (percent)
+    print ("")
+    print ('*'*80)
+    print ("Winner: " + winner)
+    print ("")
+    print ('*'*80)
+
+        
     
    
+    #write to output file
+    with open(newfilepath, 'w', newline="") as csvFile:
+        csvWriter = csv.writer(csvFile, delimiter=',')
+
+        # Write Headers into file
+        csvWriter.writerow(["Candidate","Vote Percentage","Total Votes"])
+
+        # Write the zipped lists to a csv
+        csvWriter.writerows(cleanCSV)
+
+    
+       
